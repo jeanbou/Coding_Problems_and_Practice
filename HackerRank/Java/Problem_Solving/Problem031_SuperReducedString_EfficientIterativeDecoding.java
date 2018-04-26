2887f69470eded63340981279097953eba368e19
jeanbou <ivan_budnyk@hotmail.com>
2018-04-26 15:43:23 +0200

    Problem 031 Decoding String Reducing it

---
+++ HackerRank/Java/Problem_Solving/Problem031_SuperReducedString_EfficientIterativeDecoding.java
@@ -0,0 +1,8 @@
+package hackerRank_JavaProblemSolving;
+
+public class Problem031_SuperReducedString_EfficientIterativeDecoding {
+	
+	String[] aliceItems = scanner.nextLine().split(" ");
+    scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+}
