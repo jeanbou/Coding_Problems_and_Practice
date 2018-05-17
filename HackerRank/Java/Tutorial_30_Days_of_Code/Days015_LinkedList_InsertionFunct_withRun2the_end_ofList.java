404ed7e35485d52f1a97a9b93fb94769dd232788
jeanbou <ivan_budnyk@hotmail.com>
2018-05-17 14:58:56 +0200

    Days 015 Insertion Fucntion for LinkedList with run to the end of the linkedlist and returning the very end pointer back, difficult task, code is not optimal

---
+++ HackerRank/Java/Tutorial_30_Days_of_Code/Days015_LinkedList_InsertionFunct_withRun2the_end_ofList.java
@@ -0,0 +1,59 @@
+package hackerRank_Java30DaysOfCode;
+
+import java.util.*;
+
+class Node {
+	int data;
+	Node next;
+	Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class Days015_LinkedList_InsertionFunct_withRun2the_end_ofList {
+
+	public static  Node insert(Node head,int d) {
+        
+        // CREDITS : ( if use the simple insertion )
+        // https://www.geeksforgeeks.org/linked-list-set-2-inserting-a-node/
+        // Note that in this case printing out by display method will be done in reverse order after
+        
+        // that's why I used complicated insertion then running up to the end of LinkedList
+        // note sure about more efficient solution
+        // CREDITS : https://stackoverflow.com/questions/32625964/insert-node-at-the-end-of-linked-list
+        
+        Node newNode = new Node(d);
+        if (head == null) {
+            return newNode;
+        }
+        Node current = head; 
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+        return head;
+    }
+	
+	public static void display(Node head) {
+        Node start = head;
+        while(start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int N = sc.nextInt();
+
+        while(N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head,ele);
+        }
+        display(head);
+        sc.close();
+    }
+
+}
