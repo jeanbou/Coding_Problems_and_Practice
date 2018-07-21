7cda8de916a59f3b367544d7044fb46c591165c0
jeanbou <ivan_budnyk@hotmail.com>
2018-07-21 07:56:49 +0200

    Problem 083 Array Permutation Posibility check using sorting as key

---
+++ HackerRank/Java/Problem_Solving/Problem083_PermutingTwoArrays__A_Possibility.java
@@ -0,0 +1,69 @@
+package hackerRank_JavaProblemSolving;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Problem083_PermutingTwoArrays__A_Possibility {
+
+    // Algorithmic complexity O(2nlogn)
+    // THE IDEA: it asked about possibility of existence such permutations
+    static String twoArrays(int k, int[] A, int[] B, int n) {
+        Arrays.sort(A);
+        Arrays.sort(B);
+        for (int i = 0; i < n; i++) {
+            if (A[i]+B[n-i-1] < k) {
+                return "NO";
+            }
+        }
+        return "YES";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String[] nk = scanner.nextLine().split(" ");
+
+            int n = Integer.parseInt(nk[0]);
+
+            int k = Integer.parseInt(nk[1]);
+
+            int[] A = new int[n];
+
+            String[] AItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int AItem = Integer.parseInt(AItems[i]);
+                A[i] = AItem;
+            }
+
+            int[] B = new int[n];
+
+            String[] BItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int BItem = Integer.parseInt(BItems[i]);
+                B[i] = BItem;
+            }
+
+            String result = twoArrays(k, A, B, n);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
