d943ddf391d7fc395dcc2cbf0231acbf6b5e863a
jeanbou <ivan_budnyk@hotmail.com>
2018-07-23 22:51:42 +0200

    Problem 099 Manasa and Stones, solution using Unique Set Data Accumulator of linear combination of sum of two values

---
+++ HackerRank/Java/Problem_Solving/Problem099_Manasa_andStones_UniqueSetSolutionOfLinearCombination.java
@@ -0,0 +1,58 @@
+package hackerRank_JavaProblemSolving;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Problem099_Manasa_andStones_UniqueSetSolutionOfLinearCombination {
+
+    static int[] stones(int n, int a, int b) {
+        Set<Integer> uniqueCombination = new HashSet<Integer>(); // Unique combination of linear sum that contains of all possible results 
+        for ( int i = 0; i < n; i++) {
+            uniqueCombination.add(b*i+a*(n-i-1));
+        }
+        int[] res = uniqueCombination.stream().mapToInt(Number::intValue).toArray();
+        Arrays.sort(res);
+        return res;        
+    } // stones
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int T = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int TItr = 0; TItr < T; TItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int a = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int b = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] result = stones(n, a, b);
+
+            for (int i = 0; i < result.length; i++) {
+                bufferedWriter.write(String.valueOf(result[i]));
+
+                if (i != result.length - 1) {
+                    bufferedWriter.write(" ");
+                }
+            }
+
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
